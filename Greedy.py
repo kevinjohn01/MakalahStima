c71c0eb0931721f873c395d90139632575333866
kevinjohn01 <16521168@std.stei.itb.ac.id>
2023-05-18 13:19:16 +0700

    feat greedy by rating

--- Greedy.py
+++ Greedy.py
@@ -4,46 +4,115 @@ from StringMatching import *
 def getBahanByMenu(listbahan,namamenu):
     for i in range(0,len(listbahan)):
         if (listbahan[i][0] == namamenu):
-            return listbahan[i][0]
+            return listbahan[i][1]
     return -1
 
-def greedyByPrice(filename, budget, totalparticipant, listAvoidIngredient, countavoid, max):
-    result = np.array([])
+def greedyByPrice(filename, budget, totalparticipant, listAvoidIngredient,max):
+    result = np.empty((0,2))
     listprice = getPrice(filename)
     listbahan = getBahan(filename)
     totalprice = 0
+    maxnow = max
 
     listpricesort = sortarray(listprice)
     i = 0
     while(totalparticipant > 0 and totalprice < budget):
         menu = listpricesort[i][0]
-        print(f"cek {menu}")
+        #print(f"cek {menu}")
         bahan = getBahanByMenu(listbahan,menu)
         if (bahan != -1):
             if (fungsikendala(listAvoidIngredient,bahan) == False):
-                result = np.append(result,menu)
+                result = addMenu(result,menu)
                 totalparticipant -= 1
                 totalprice += listpricesort[i][1]
-                print(f"{menu} berhasil ditambahkan")
+                #print(f"{menu} berhasil ditambahkan")
+                maxnow -=1
+                if (maxnow == 0):
+                    maxnow = max
+                    i += 1
             else:
-                countavoid -= 1
-                print(f"{menu} mengandung bahan yang tak diinginkan")
+                i += 1
+                #print(f"{menu} mengandung bahan yang tak diinginkan")
         
-        i += 1
         if (i>len(listpricesort)-1):
             i = 0
+    if (totalprice > budget):
+        totalprice -= listpricesort[i][1]
+        result = removeLastElement(result)
     return result, totalprice
+
+def greedyByRating(filename, totalparticipant, listAvoidIngredient,max):
+    result = np.empty((0,2))
+    listrating = getRating(filename)
+    listbahan = getBahan(filename)
+    meanrating = 0
+    maxnow = max
+
+    listratingsort = sortarray(listrating)
+    i = 0
+    while(totalparticipant > 0):
+        menu = listratingsort[i][0]
+        #print(f"cek {menu}")
+        bahan = getBahanByMenu(listbahan,menu)
+        if (bahan != -1):
+            if (fungsikendala(listAvoidIngredient,bahan) == False):
+                result = addMenu(result,menu)
+                totalparticipant -= 1
+                meanrating = (meanrating*(len(result)-1)+listratingsort[i][1])/(len(result))
+                #print(f"{menu} berhasil ditambahkan")
+                maxnow -=1
+                if (maxnow == 0):
+                    maxnow = max
+                    i += 1
+            else:
+                i += 1
+                #print(f"{menu} mengandung bahan yang tak diinginkan")
+        
+        if (i>len(listratingsort)-1):
+            i = 0
+        
+    return result, meanrating
         
 def fungsikendala(avoidIngredients,bahan):
+    #print(bahan)
     arrayAvoid = avoidIngredients.split()
-    print(arrayAvoid)
+    #print(arrayAvoid)
     avoid = False
     for avoid in arrayAvoid:
-        print(boyermoore(bahan,avoid))
+        #print(avoid)
+        #print(boyermoore(bahan,avoid))
         if (boyermoore(bahan,avoid) == -1):
             avoid = False
         else:
             avoid = True
             break
-    print(avoid)
+    #print(avoid)
     return avoid
+
+def addMenu(listRecommendation,menu):
+    found = False
+    #print(listRecommendation)
+    if (len(listRecommendation) != 0):
+        for i in (0,len(listRecommendation)-1):
+            #print(listRecommendation[i,0])
+            if (listRecommendation[i,0] == menu):
+                listRecommendation[i,1] = int(listRecommendation[i,1]) + 1
+                found = True
+                break
+    if (found == False):
+        array = np.array([menu,1])
+        listRecommendation = np.append(listRecommendation,[array],axis=0)
+    return listRecommendation
+
+def printMenu(listRecommendation):
+    print("  Menu - Quantity")
+    for i in range (0,len(listRecommendation)):
+        print(f"{i+1}. {listRecommendation[i][0]} - {listRecommendation[i][1]}")
+
+def removeLastElement(listRecommendation):
+    i = len(listRecommendation)-1
+    if (int(listRecommendation[i,1]) >1):
+        listRecommendation[i,1] = int(listRecommendation[i,1]) -1
+    else:
+        listRecommendation = listRecommendation[:-1]
+    return listRecommendation
--- main.py
+++ main.py
@@ -4,35 +4,50 @@ from Greedy import *
 file = input("Masukkan nama file (tanpa extension): ")
 
 print("----------------SELAMAT DATANG----------------")
-countOrang = int(input("Masukkan berapa orang untuk rekomendasi menu ini: "))
-inp = input("Apakah ada bahan yang ingin anda hindari? (y/n): ")
-avoidIngredients = ""
-countAvoid = 0
-if (inp == 'y'):
-    avoidIngredients = input("Apakah anda menghindari bahan tertentu? Silakan masukkan disini: ")
-    countAvoid = int(input("Bahan ini dihindari untuk berapa orang? "))
-print("Tentukan prioritas pemilihan menu untuk Anda")
-print("1. Berdasarkan harga")
-print("2. Berdasarkan rating")
-print("3. Berdasarkan kalori")
-print("4. Berdasarkan rating/harga")
-print("5. Berdasarkan popularitas")
-print("6. Berikan saya menu random")
+run = True
+while(run):
+    countOrang = int(input("Masukkan berapa orang untuk rekomendasi menu ini: "))
+    inp = input("Apakah ada bahan yang ingin anda hindari? (y/n): ")
+    avoidIngredients = ""
+    #countAvoid = 0
+    if (inp == 'y'):
+        avoidIngredients = input("Apakah anda menghindari bahan tertentu? Silakan masukkan disini: ")
+        #countAvoid = int(input("Bahan ini dihindari untuk berapa orang? "))
+    print()
+    max = 1
+    inp = input("Apakah anda ingin semua menu berbeda? (y/n): ")
+    if (inp == 'n'):
+        max = int(input("Berapa maksimal kuantitas per menu yang anda inginkan? "))
+    print()
+    print("Tentukan prioritas pemilihan menu untuk Anda")
+    print("1. Berdasarkan harga")
+    print("2. Berdasarkan rating")
+    print("3. Berdasarkan kalori")
+    print("4. Berdasarkan rating/harga")
+    print("5. Berdasarkan popularitas")
+    print("6. Berdasarkan popularitas*rating")
+    print("6. Berikan saya menu random")
+    print()
+    listRecommendation = []
+    inp = int(input())
+    if (inp == 1):
+        budget = int(input("Tentukan budget anda: "))
+        listRecommendation, totalprice = greedyByPrice(file,budget,countOrang,avoidIngredients,max)
+        print("Berikut ini adalah menu yang kami rekomendasikan berdasarkan masukan Anda")
+        printMenu(listRecommendation)
+        print()
+        print(f"Dengan total harga {totalprice}")
+    elif (inp == 2):
+        listRecommendation, meanrating = greedyByRating(file,countOrang,avoidIngredients,max)
+        print("Berikut ini adalah menu yang kami rekomendasikan berdasarkan masukan Anda")
+        printMenu(listRecommendation)
+        print()
+        print(f"Dengan total harga {meanrating}")
+    #elif (inp == 3):
+    #    listRecommendation = greedyByCalories(file)
+    #else:
+    #    listRecommendation = greedyByRatePerPrice(file)
+    inp = input("Apakah Anda puas dengan rekomendasi ini? Jika tidak anda bisa mengulangnya (y/n): ")
+    if (inp == 'n'):
+        run = False
 
-listRecommendation = []
-inp = int(input())
-if (inp == 1):
-    budget = int(input("Tentukan budget anda: "))
-    listRecommendation, totalprice = greedyByPrice(file,budget,countOrang,avoidIngredients,countAvoid)
-#elif (inp == 2):
-#    listRecommendation = greedyByRating(file)
-#elif (inp == 3):
-#    listRecommendation = greedyByCalories(file)
-#else:
-#    listRecommendation = greedyByRatePerPrice(file)
-print("Berikut ini adalah menu yang kami rekomendasikan berdasarkan masukan Anda")
-print(listRecommendation)
-print()
-print(f"Dengan total harga {totalprice}")
-if (len(listRecommendation) < countOrang):
-    print("Maaf, dengan syarat yang anda berikan, kami belum bisa menampilkan rekomendasi agar semua orang dapat makanan")
